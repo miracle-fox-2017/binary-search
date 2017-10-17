e0ff123b36a7085843bdabc5c9b0ee7399220396
Faris Widyantho <radenfaris@gmail.com>
2017-10-17 16:00:58 +0700

    release finish

--- binary.js
+++ binary.js
@@ -1,22 +1,107 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8];
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55];
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  //selection sort, ambil yang terkecil, tampung di tempat yang baru
+  var pointer = 0;
+  var kecil = 0;
+  var besar = 0;
+  var sorted = [];
+  var loops = arr.length;
+  while(loops>1){
+    //cari yang kecil
+    if(arr.length>2){
+      kecil = arr[0];
+      for(var i = 0; i<arr.length; i++){
+        if(kecil>arr[i]){
+          kecil = arr[i];
+          pointer = i;
+        }
+      }
+
+    //push ke tempat baru
+    sorted.push(kecil);
+
+    } else {
+      if(arr[0]>arr[1]){
+        kecil = arr[1];
+        besar = arr[0];
+      } else {
+        kecil = arr[0];
+        besar = arr[1];
+      }
+      sorted.push(kecil);
+      sorted.push(besar);
+      break;
+    }
+
+    //hapus yang terkecil dari array
+    arr.splice(pointer,1);
+    pointer = 0;
+
+    loops -= 1;
+
+  }
+
+  return sorted;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  //min, max, max
+  debugger;
+  var min = 0;
+  var max = array.length;
+  var mid = Math.floor(array.length/2);
+  var count = 0;
+
+  //if mid == search.... return mid
+  if(array[mid] == search){
+    return mid;
+  }
+
+  while(array[mid] != search){
+    //if search > mid.... masuk geng besar
+    if(search > array[mid]){
+      //geng besar
+      //max = tetep, min = mid, mid = max+min/2
+      min = mid;
+      mid = Math.floor((max+min)/2);
+      //cek if mid == search return mid
+      if(array[mid] == search){
+        return mid;
+      }
+    } else {
+    //else search < mid.... masuk geng kecil
+      //geng kecil
+      //max = mid, min = tetep, mid = max+min/2
+      max = mid;
+      mid = Math.floor((max+min)/2);
+      //cek if mid == search return mid
+      if(array[mid] == search){
+        return mid;
+      }
+    }
+
+    //if jumlah looping > panjang array return -1
+    if(count>array.length){
+      return -1;
+    }
+
+    count += 1;
+  }
+
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
+// console.log(arrayGenapSorted)
+// console.log(arrayGanjilSorted)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
