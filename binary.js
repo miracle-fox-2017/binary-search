0ae4fa155b4ad466960c20914a78c95a6f045b9f
Ayunda <ayundahamzah@gmail.com>
2017-10-17 23:02:25 +0700

    sort dan search

--- binary.js
+++ binary.js
@@ -4,13 +4,48 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < i; j++){
+      // console.log('masuk for ke 2')
+      if(arr[i] < arr[j]){
+        var tampung = 0;
+        tampung = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tampung;
+      }
+   }
+ }
+ return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+
+
+function binary_search (search, array) {
+    var start = 0;
+    var end = array.length - 1;
+    var mid = Math.floor((start + end) / 2);
+    while (true)
+    {
+      if (search === array[mid])
+      {
+        return mid;
+      }
+      else if (search < array[mid])
+      {
+        end = mid - 1;
+      }
+      else if (search > array)
+      {
+        start = mid + 1;
+      }
+      else
+      {
+          return -1;
+      }
+      mid = Math.floor((start + end) / 2);
+
+    }
+    return 0;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -25,6 +60,6 @@ console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
 
-module.exports = {
-  binary_search
-}
+// module.exports = {
+//   binary_search
+// }
