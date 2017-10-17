c8d2c633b84f7430739a25fa8165d3356d09759a
agungpangestu <agung_pangestu83@ymail.com>
2017-10-17 22:36:33 +0700

    Done

--- binary.js
+++ binary.js
@@ -3,18 +3,52 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
-function ownSort(arr) {
-  // Your sorting code
-  return arr
+  function ownSort(arr) {
+    // Your sorting code
+  let temp;
+  for (let i = 0 ; i <arr.length ; i++){
+    for (let j =0 ; j< arr.length; j++){
+      if (arr[i]<arr[j]){
+        temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp
+      }
+    }
+  }
+    return arr
+  }
+
+
+function binary_search (search, array) {
+  // Your searching code
+  let start = 0 ;
+  let end = array.length-1;
+  let mid = Math.floor((start + end)/2)
+
+while (start <= end) {
+  // console.log('masuk sini');
+// debugger
+  if (search < array[mid]){
+      end = mid - 1
+      mid = Math.floor((start + end)/2)
+    }
+  else if (search > array[mid]){
+      start = mid + 1
+      mid = Math.floor((start + end)/2)
+    }
+    else {
+        return mid
+    }
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+// console.log(arrayGanjilSorted);
+// console.log(arrayGenapSorted);
+
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
