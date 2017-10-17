fcbba14d514999b4f34e7c71bc101279b4847600
aditReza <lete.note3@gmail.com>
2017-10-17 16:27:09 +0700

    release 1

--- binary.js
+++ binary.js
@@ -14,26 +14,41 @@ function ownSort(arr) {
       }
     }
   }
-  // console.log(arr)
+  console.log(arr)
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let start = 0
+  let end   = array.length
+  // console.log(end + ' < length======arr > ' + array)
+
+  while(start <= end){
+    let mid = Math.floor((start + end)/2)
+    // console.log('-=-=-=-' + array[mid])
+    if(array[mid] === search){
+      return `${true} | indeks = ${mid}`
+    }else if(search > array[mid]){
+      start = mid + 1
+    }else if(search < array[mid]){
+      end = mid - 1
+    }
+  }
+  return `${false} -1`
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-// console.log(binary_search(8, arrayGenapSorted))
-// console.log(binary_search(10, arrayGenapSorted))
-// console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(10, arrayGenapSorted))
+console.log(binary_search(33, arrayGenapSorted))
 //
-// console.log(binary_search(53, arrayGanjilSorted))
-// console.log(binary_search(3, arrayGanjilSorted))
-// console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted))
+console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = {
   binary_search
