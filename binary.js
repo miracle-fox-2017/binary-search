9287eb24f2ce3607f7de9f969c0277c43a0cd3bd
riskinputra <riski.n.putra@gmail.com>
2017-10-17 20:48:47 +0700

    Update tanpa sort functioin

--- binary.js
+++ binary.js
@@ -6,9 +6,17 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 function ownSort(arr) {
   // Your sorting code
   debugger;
-  arr = arr.sort(function(value1, value2) {
-    return value1 > value2
-  });
+  let hasil = 0;
+
+  for (var i = 0 ; i < arr.length ; i++){
+  	for (var j = i ; j < arr.length ; j++){
+  		if (arr[i] >= arr[j]){
+  			hasil=arr[i];
+  			arr[i]=arr[j];
+  			arr[j]=hasil;
+  		}
+  	}
+  }
   return arr
 }
 
@@ -19,13 +27,17 @@ function binary_search (search, array) {
   let end = array.length-1;
   let mid = Math.round(end/2);
   let hasil = 0
+
   while (search !== array[mid]){
+    debugger;
   	if (search > array[mid]){
+      debugger;
   		start = end+1;
   		end = array.length;
       hasil = start + end;
   		mid = Math.floor((hasil)/2);
   	}else if (search < array[mid]){
+      debugger;
   		start = 0;
   		end = mid-1;
       hasil = (start + end)/2;
