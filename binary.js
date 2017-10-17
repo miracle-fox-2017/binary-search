e6eadf320baad6960e35dac199f0c746fd3f93bd
Rizky <co.terbang@gmail.com>
2017-10-17 15:52:39 +0700

    binary.js done

--- binary.js
+++ binary.js
@@ -3,13 +3,49 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
-function ownSort(arr) {
-  // Your sorting code
-  return arr
+function ownSort(arr)
+{
+  let temp = 0;
+  for (var i = 0; i < arr.length - 1; i++)
+  {
+    while (arr[i] > arr[i + 1] && i >= 0)
+    {
+      temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+      i--;
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
+function binarySearch (search, array)
+{
+  let start = 0;
+  let end = array.length - 1;
+  let mid = Math.floor((start + end) / 2);
+  while (true)
+  {
+    debugger;
+    if (search === array[mid])
+    {
+      return mid;
+    }
+    else if (search < array[mid])
+    {
+      end = mid - 1;
+    }
+    else if (search > array)
+    {
+      start = mid + 1;
+    }
+    else
+    {
+        return -1;
+    }
+    mid = Math.floor((start + end) / 2);
+
+  }
   return 0;
 }
 
@@ -17,14 +53,14 @@ var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
+//
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+// module.exports = {
+//   binary_search
+// }
