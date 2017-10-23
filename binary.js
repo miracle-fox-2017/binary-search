ebdbd2a553f4c90ec33a5c1ef3837eb0f7f74550
albertagung <albiardtya@gmail.com>
2017-10-23 18:35:04 +0700

    done

--- binary.js
+++ binary.js
@@ -4,13 +4,37 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  for(var i = 1; i <= arr.length-1; i++){
+    for(var j = 0; j < i - 1; j++){
+      if(arr[i] < arr[j]){
+        var tampung = arr[i]
+        arr[i] = arr[j];
+        arr[j] = tampung;
+      }
+    }
+  }
+  console.log(arr)
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var highIndex = array.length;
+  var startIndex = 0;
+  while(startIndex <= highIndex){
+    var midIndex = Math.floor((startIndex + highIndex)/2);
+    console.log(midIndex);
+    if(array[midIndex] === search){
+      return midIndex;
+    }
+    else if(array[midIndex] < search){
+      startIndex = midIndex+1
+    }
+    else if(array[midIndex] > search){
+      highIndex = midIndex-1
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
